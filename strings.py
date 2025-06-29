0f61ed63608416dd34b3e3d06c0eb959f8d4b1e0
ngcwangusanele <ngcwangusanele@gmail.com>
2025-06-29 17:42:54 +0200

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,9 @@
+#Advanced concepts - Strings
+
+message = ' Hello, World! '
+
+print(message.strip())
+print(message.lower())
+print(message.split())
+print(message.upper())
+print(message.replacemethod())
